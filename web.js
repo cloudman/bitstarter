9125d56842c6b17ec7d62aeb04e8ffcf4b9e1152
Christopher Goodman <cloudman@mac.com>
2013-07-11 23:20:37 -0500

    first commit part one bitstarter
    hello world 2

--- web.js
+++ web.js
@@ -1,17 +1,12 @@
 var express = require('express');
-var fs = require('fs');
-
-// Get contents of index.html
-var contents = fs.readFileSync('index.html');
-//console.log('Contents of index.html:' + contents.toString('utf-8'));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(contents.toString('utf-8'));
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
